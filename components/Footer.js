48ec596b6a09587b3a114b3a2c7293717fde103b
Pedro <web@agenciaade.com.br>
2022-08-19 07:14:59 -0300

    Subindo components

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <img src="/FooterIMG.png" alt="" className='tw-w-full tw-h-min' />
+        </>
+    )
+}
+
+export default Footer
